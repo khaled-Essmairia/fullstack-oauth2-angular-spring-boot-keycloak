f58839530999ae823b14efc4c0a3066e566d58d6
essmairia.kaled@gmail.com <essmairia.kaled@gmail.com>
2024-02-16 00:21:34 -1100

    setup

---
+++ backenkeycloak/src/main/java/com/backendkeycloak/backenkeycloak/BackenkeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.backendkeycloak.backenkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenkeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenkeycloakApplication.class, args);
+	}
+
+}
---
+++ backenkeycloak/src/test/java/com/backendkeycloak/backenkeycloak/BackenkeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendkeycloak.backenkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenkeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
